6607d1f01006ea222e3e6e3df7d49de9b84a1a2f
Luiz Sócrate <luiz@socrate.com.br>
2012-11-25 21:38:45 -0200

    Add admin menu items

--- tech-visualization.php
+++ tech-visualization.php
@@ -1,10 +1,28 @@
 <?php
-/**
- * @package Tech Visualization
- */
 /*
 Plugin Name: Tech Visualization
 Version: 0.1
 Author: Luiz Sócrate
 Author URI: http://socrate.com.br
 */
+
+new TechVisualizations();
+class TechVisualizations {
+    const NAME = "Visualizations";
+    const SLUG = "techVisualization";
+
+    public function __construct() {
+        add_action("admin_menu", array(&$this, "add_menu_page"));
+    }
+
+    public function add_menu_page() {
+        add_menu_page(self::NAME, self::NAME, 'edit_posts', self::SLUG, array(&$this, "showVisualizationPage"), null, 24);
+        add_submenu_page(self::SLUG, self::NAME, "Tech Content", "edit_posts", "techContent", array(&$this, "showTechContent"));
+    }
+
+    public function showVisualizationPage() {
+    }
+
+    public function showTechContent() {
+    }
+}
