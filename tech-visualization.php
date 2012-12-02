5ba406b794d689eec5a785ebc27deed5a55dd370
Luiz Sócrate <luiz@socrate.com.br>
2012-12-02 18:11:57 -0200

    Visualization upload

--- tech-visualization.php
+++ tech-visualization.php
@@ -45,6 +45,65 @@ class TechVisualizations {
     }
 
     public function showVisualizationPage() {
+        wp_enqueue_style('imgareaselect');
+        wp_enqueue_script('plupload-handlers');
+        wp_enqueue_script('image-edit');
+        wp_enqueue_script('set-post-thumbnail');
+        wp_enqueue_script('media-gallery');
+
+        $_GET["inline"] = "true";
+
+        @header('Content-Type: ' . get_option('html_type') . '; charset=' . get_option('blog_charset'));
+
+        $errors = array();
+
+        if ( isset($_POST['html-upload']) && !empty($_FILES) ) {
+            check_admin_referer('media-form');
+            // Upload File button was clicked
+            $id = media_handle_upload('async-upload', $_REQUEST['post_id']);
+            unset($_FILES);
+            if ( is_wp_error($id) ) {
+                $errors['upload_error'] = $id;
+                $id = false;
+            }
+        }
+
+        if ( isset($_GET['upload-page-form']) ) {
+            $errors = array_merge($errors, (array) media_upload_form_handler());
+
+            $location = 'upload.php';
+            if ( $errors )
+                $location .= '?message=3';
+
+            wp_redirect( admin_url($location) );
+            exit;
+        }
+        ?>
+        <div class="wrap">
+        <h2>Upload New Visualization</h2>
+
+        <form enctype="multipart/form-data" method="post" action="<?php echo admin_url('admin.php?page=techVisualization&amp;inline=&amp;upload-page-form='); ?>" class="media-upload-form type-form validate html-uploader" id="file-form">
+
+        <?php media_upload_form(); ?>
+
+        <script type="text/javascript">
+        jQuery(function($){
+            var preloaded = $(".media-item.preloaded");
+            if ( preloaded.length > 0 ) {
+                preloaded.each(function(){prepareMediaItem({id:this.id.replace(/[^0-9]/g, '')},'');});
+            }
+            updateMediaForm();
+            post_id = 0;
+            shortform = 1;
+        });
+        </script>
+        <input type="hidden" name="post_id" id="post_id" value="0" />
+        <?php wp_nonce_field('media-form'); ?>
+        <div id="media-items" class="hide-if-no-js"></div>
+        <?php submit_button( __( 'Save all changes' ), 'button savebutton hidden', 'save' ); ?>
+        </form>
+        </div>
+        <?php
     }
 
     public function showTechContent() {
